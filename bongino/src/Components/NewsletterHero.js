e9d57fa46d868e69b07ebdd807605924b20f48b0
Matt <MVSucharski@gmail.com>
2022-05-07 15:50:10 -0400

    made new components

---
+++ bongino/src/Components/NewsletterHero.js
@@ -0,0 +1,29 @@
+import React, {
+    Component
+} from 'react';
+//import { Link } from 'react-router-dom';
+
+
+export default class NewsletterHero extends Component {
+    render() {
+        return ( 
+            <div>
+            <header className="header large">
+    <div>
+        <div ></div>
+        <div className="header__sticky-wrapper" >
+            
+            <div id="billboard_1"></div>
+        </div>
+    </div>
+    <div className="header__title">
+        <div className="container">
+            <h1>Newsletter</h1>
+            <h2>Sign Up</h2>
+        </div>
+    </div>
+</header>
+            </div>
+        )
+    }
+}
--- bongino/src/Components/NewsletterLayout.js
+++ bongino/src/Components/NewsletterLayout.js
@@ -3,17 +3,18 @@ import React, {
 } from 'react';
 //import { Link } from 'react-router-dom';
 
-
+import NewsletterHero from "./NewsletterHero";
 import OnTheAir from "./OnTheAir";
 import ParlerBanner from "./ParlerBanner";
 import LatestFeatured from "./LatestFeatured";
+import LatestPodcasts from "./LatestPodcasts";
 
 export default class Navbar extends Component {
     render() {
         return ( 
             <div>
+            <NewsletterHero />
             <OnTheAir />
-            <ParlerBanner />
         </div>
         )
     }
